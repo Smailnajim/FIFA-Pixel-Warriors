eb4f080918667475226715028d375ec12eed53b1
smailnajim <smailnajim>
2024-11-26 14:14:38 +0100

    firt etap, commit file html and css

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//creat div camla                    |T a 9 t e   l a 3 i b        |i m e g e s                    |...
+const divIn$sectionPlyers = function(pac, sho, pas, dri, def, phy, imgJoure, imgMontakhb, imgnadi, isme, balad, nadi, posi) {
+    
+}
+
+
+fetch("js/main.json").then(response => response.json()).then(obj => {//start fetch
+    const sectionPlyers = document.querySelector(".player"); //jbt sction dyal l3aba
+
+});//fine fetch
